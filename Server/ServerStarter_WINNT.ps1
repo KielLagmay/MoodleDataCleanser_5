b785531a67c31d5189a9603464b20d1a839e22e3
KielLagmay <37663637+KielLagmay@users.noreply.github.com>
2021-07-07 16:51:25 +0800

    Add files via upload

---
+++ Server/ServerStarter_WINNT.ps1
@@ -0,0 +1,5 @@
+﻿# Sources:
+# https://stackoverflow.com/questions/27277701/powershell-get-ipv4-address-into-a-variable
+
+$ipV4 = Test-Connection -ComputerName (hostname) -Count 1  | Select -ExpandProperty IPV4Address
+pyro5-ns -n $ipV4.IPAddressToString
